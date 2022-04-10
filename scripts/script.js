2801feb667a1c4c28ae0f5264d710e98a22a5368
Daniel Schaeffer <daneschaeffer@hotmail.com>
2022-04-10 00:47:17 -0300

    newContent

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
 const buzz = document.querySelector(".character");
 const buzzDefault = document.querySelector(".buzzDefault");
-let spell = document.querySelector(".spell");
 const chat = document.querySelector(".chat");
+const status = document.querySelector(".buzzStatus");
+const mana = document.querySelector(".buzzMana");
+let spell = document.querySelector(".spell");
+let manaPoints = 100;
 
 function walk(value, side) {
   let position = buzz.getBoundingClientRect();
-  let buzzMovements = ["buzzAA", "buzzW"];
-
   let moveBuzz = position.x;
   const moveSide = (moveBuzz += value);
   buzz.style.transition = "all 0.5s";
+  let buzzMovements = ["buzzAA", "buzzW"];
 
   if (moveSide <= 0) {
     moveSide != -1;
@@ -19,6 +21,7 @@ function walk(value, side) {
     buzz.style.transform = `translateX(${moveSide}px)`;
     buzzDefault.style.transform = `scaleX(${side * -1})`;
     buzz.scroll(value, 0);
+
     for (let buzzTypes of buzzMovements) {
       let type = document.querySelector(`.${buzzTypes}`);
       type.style.transform = `scaleX(${side})`;
@@ -83,16 +86,45 @@ function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+function manaStatus() {
+  const power = document.querySelector(".character > div");
+
+  let manaRegen = setInterval(() => {
+    if (manaPoints < 100) {
+      return mana.style.width = `${manaPoints += 10}%`;
+    } else {
+      window.clearInterval(manaRegen);
+    }
+  }, 20000);
+
+  if (power) {
+    mana.style.width = `${manaPoints -= 30}%`;
+  }
+  
+}
+
+function cheats() {
+  if(chat.value == "manafull" ) {
+    return mana.style.width = `${manaPoints = 100}%`;
+  }
+}
+
 function boltPassive() {
   const buzzW = document.querySelector(".buzzW");
-  let spell = document.querySelector(".spell");
+  const manaPercentage = mana.getBoundingClientRect();
+  let manaLeft = manaPercentage.width;
 
-  if (buzzDefault.classList.contains("invisible")) {
+  if (buzzDefault.classList.contains("invisible") || manaPoints < 30) {
     return;
   } else {
+    let div = createItem("div");
+    renderItem(buzz, div);
     buzzW.classList.add("visible");
     buzzDefault.classList.add("invisible");
+    const spell = document.querySelector(".character div");
+    spell.classList.add("spell");
     spell.classList.add("thunderBlast");
+    manaStatus();
 
     for (let i = 0; i < randomNumber(30, 50); i++) {
       let img = createItem("img");
@@ -100,10 +132,9 @@ function boltPassive() {
       let randomX = randomNumber(-10, 130) - randomNumber(-10, 60);
       let randomY = randomNumber(1, 100) - randomNumber(1, 80);
       let randomR = randomNumber(1, 180) - randomNumber(1, 180);
-      let randomRotate = parseInt(randomR);
-      let convertX = parseInt(randomX);
-      let convertY = parseInt(randomY);
-      img.style.transform = `translate(${convertX}px, ${convertY}px) rotate(${randomRotate}deg)`;
+      img.style.transform = `translate(${parseInt(randomX)}px, ${parseInt(
+        randomY
+      )}px) rotate(${parseInt(randomR)}deg)`;
       renderItem(spell, img);
     }
 
@@ -122,11 +153,13 @@ function boltPassive() {
   }
 
   let cleanBolt = setInterval(() => {
-    if (!buzzDefault.classList.contains("invisible")) {
-      let imgs = document.querySelectorAll(".spell img");
-      return imgs.forEach((e) => spell.removeChild(e));
+    const power = document.querySelector(".character > div");
+
+    if (power) {
+      power.remove();
+      Array.from(power).forEach((e) => spell.removeChild(e));
+      window.clearInterval(cleanBolt);
     }
-    window.clearInterval(cleanBolt);
   }, 4000);
 }
 
@@ -153,17 +186,36 @@ function chatRender() {
 
   window.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
-      chat.focus();
       divMain.classList.remove("invisible");
       divMain.classList.add("visible");
       let p = createItem("p");
       renderItem(div, p);
-      p.innerHTML = chat.value;
-      chat.value = "";
+
+      if (e.key == "Enter") {
+        p.innerHTML = chat.value;
+        cheats()
+        chat.value = "";
+      }
+
       setInterval(() => {
         p.innerHTML = "";
       }, 10000);
     }
+
+    return chat.focus();
+  });
+
+  chat.addEventListener("blur", () => {
+    let p = document.querySelector(".chatRender p");
+
+    setInterval(() => {
+      if (p.innerHTML == "") {
+        divMain.classList.remove("visible");
+        divMain.classList.add("invisible");
+      } else {
+        return;
+      }
+    }, 5000);
   });
 }
 
